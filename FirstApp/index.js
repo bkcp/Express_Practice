e458b82e3e76a962b42e89ebb284d18a445acb2f
bkcp <bkcp83@gmail.com>
2022-05-21 17:07:54 -0700

    app.get and app.post

--- FirstApp/index.js
+++ FirstApp/index.js
@@ -1,12 +1,35 @@
 const express = require('express');
 const app = express();
 
-app.use((req,res)=>{
-    res.send("<h1>this is the response</h1>")
-    //res.send({color: 'red'});
-    // console.log(res)
-})
+// app.use((req,res)=>{
+//     res.send("<h1>this is the response</h1>")
+//     //res.send({color: 'red'});
+//     // console.log(res)
+// })
 
 app.listen(3000,()=>{
     console.log('Listening on Port 3000')
+})
+
+
+// /cats => 'meow'
+// /dogs => 'woof'
+
+app.get('/',(req,res)=>{
+    res.send('This is home page')
+})
+app.post('/cats',(req,res)=>{
+    res.send("this is a post request")
+})
+app.get('/cats',(req,res)=>{
+console.log('cat request!');
+res.send('meow');
+})
+
+app.get('/dogs',(req,res)=>{
+    res.send('woof');
+})
+
+app.get('*',(req,res)=>{
+    res.send('idunno that path!')
 })
